f156a7d4c57fbe1b81354bb736dac8a6b353fee5
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-11-08 10:01:15 +0000

    Add files via upload
    MouseX and Mouse Y improvements

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,32 @@
+//code not in a group at the top of the page is global and change all affected code in the project
+//known as Scope
+var firstName = "indy"
+var thisIsAstrig = 'hello'
+var thisIsAnInteger
+var thisIsAnUndefinedVar;
+var thsisNull = null;
+
+var ellipseX = 400;
+var ellipseY = 400;
+
+//‘var’ and ‘const’ are similar - const is one unchangeable value which is easier on RAM and can't be used again in the code, and var an be changed
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 600);
     canvas.parent("p5container");
+    console.log(firstName);
+    console.log(thisIsAnInteger)
+    background(220);
 }
 
+
+
 function draw() {
-    background(220);
+   
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(mouseX, mouseY, 100);
+
+    console.log(width, height);
+    
 }
